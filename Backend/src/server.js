498227dba5d58908e3fec89395718c2ab1efcae4
Noor ul saqlain <noorulsaqlain3@email.com>
2026-03-13 12:13:43 +0500

    importing functions

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { Serve } from "inngest/express";
-import { inngest } from "./lib/inngest.js";
+import { functions, inngest } from "./lib/inngest.js";
 
 const app = express();
 
