be5be06fa1e520276d848a01d2cd7ad7cd8e251a
Noor ul saqlain <noorulsaqlain3@email.com>
2026-04-16 22:41:10 +0500

    setup the all frontend tools

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,31 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
-import './App.css'
-import {Show, SignInButton, SignOutButton,UserButton,} from "@clerk/react"
+import ProblemsPage from "./pages/ProblemsPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const { isSignedIn } = useUser();
 
   return (
     <>
-    <h1>welcome to my app</h1>
-      <Show when="signed-out">
-        <SignInButton mode="modal"> 
-          Please sign In
-        </SignInButton>
-      </Show>
-      <Show when="signed-in">
-        <SignOutButton />
-      </Show>
-      <UserButton />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+          <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+      </Routes>
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
-  )
+  );
 }
+export default App;
 
-export default App
+// tw, daisyui, react-router, react-hot-toast,
+// todo: react-query aka tanstack query, axios
