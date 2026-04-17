1a290a6e42e4fdf375911cd1228e2ed81507781e
Noor ul saqlain <noorulsaqlain3@email.com>
2026-04-17 21:58:17 +0500

    problem page details added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,6 +12,7 @@ import DashboardPage from "./pages/DashBoardOage";
 
 import ProblemsPage from "./pages/ProblemsPage";
 import { Toaster } from "react-hot-toast";
+import ProblemPage from "./pages/ProblemPage";
 
 function App() {
  const { isSignedIn, isLoaded } = useUser();
@@ -25,6 +26,7 @@ function App() {
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
           <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+          <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
       </Routes>
       <Toaster toastOptions={{ duration: 3000 }} />
     </>
